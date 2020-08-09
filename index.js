d69da04a5592477098aa8addb1c2e3dc0b168419
elandau91 <eli.b.landau@gmail.com>
2020-08-09 15:13:03 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let bigGame = array.find(game => game.result === "W")
+      
+  if (bigGame !== undefined) {
+    return bigGame.year
+  } else {
+    return undefined
+  }
+  
+  
+}
